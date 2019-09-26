321f37e1058449f192bfb1e3db1360cbefdc00be
Solovev-Sergey <s.solovev1@g.nsu.ru>
2019-09-26 15:00:51 +0700

    solution

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 using namespace std;
 long long int fibonachi(int number){
-    long long int a = 1,b = 1;
-    for(int i=0;i < number;i++){
+    long long int a = 1, b = 0;
+    for(int i = 0; i < number; i++){
+        int k = b;
         b = a + b;
-        a = b - a;
+        a = k;
     }
     return b;
 }
