e802713b3319c9d651c185c6e23c8f3c12027522
unknown <lizabotnikova@mail.ru>
2019-09-26 14:58:00 +0700

    solution

--- main.cpp
+++ main.cpp
@@ -7,9 +7,9 @@ int main()
      data[1]=1;
     long long int i;
     cin>>i;
-    if (i>2)
+    if (i>1)
     {
-        for (long long int j=3; j<=i; j++)
+        for (long long int j=2; j<=i; j++)
         {
             data[2]=data[1]+data[0];
             data[0]=data[1];
@@ -17,7 +17,7 @@ int main()
         }
     }else
     {
-        data[2]=data[i-1];
+        data[2]=data[i];
     }
     
     cout<<data[2]<<endl;
