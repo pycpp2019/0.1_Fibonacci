3b235a3ee6ab8af5373dc39cbc5fd803d9794fff
well120 <n.ushakova1@g.nsu.ru>
2019-10-03 14:46:35 +0700

    fix

--- main.cpp
+++ main.cpp
@@ -1,21 +1,22 @@
 #include <iostream>
 using namespace std;
 
-int F(int n) {
-    int a = 0, b = 1, i;
+long long int F(long long int n) {
+    long long int a = 0, b = 1, i;
     if (i <= 2) {
         return 1;
     } else {
         for (i = 3; i <= n; i++) {
+            long long int c = a;
             a = a + b;
-            b = a - b;
+            b = c;
         }
         return a;
     }
 }
 
 int main() {
-    int n;
+    long long int n;
     cin >> n;
     cout << F(n) << endl;
     return 0;
